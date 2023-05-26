9eb91544cfd5705b8aafb7a3894dd3e9e4edfbff
Kaushalgurung <kaushalgurung2057@gmail.com>
2023-05-26 21:34:01 +0545

    Freelance work

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+const slider1 = document.querySelector('#glide_1');
+if (slider1) {
+  new Glide(slider1, {
+    type: 'carousel',
+    startAt: 0,
+    gap: 0,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: 'linear',
+  }).mount();
+}
